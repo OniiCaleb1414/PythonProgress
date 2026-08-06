3fa639212d1791485a18042db63f0d3d2265b82d
OniiCaleb1414 <belaycaleb14@gmail.com>
2026-08-06 22:32:03 +0200

    Made a bit of progress in python syntax and syllabes

---
+++ simplecalc.py
@@ -0,0 +1,44 @@
+print('Rico is the best')
+
+
+
+x = 5
+y = 7
+
+z = x + y
+
+# print(z)
+
+# print(type(3+5j))
+
+p = "Caleb Belay"
+
+# print(p[1:8])
+print(p.split(" "))
+# }
+
+    # Here I have a simple addition process but it takes the integers as strings
+
+print("What is the first number?: ")
+x = input()
+print("What is the second number?: ")
+y = input()
+z = x+y
+print("The sum is: " + z)
+
+import math
+
+print(math.sin(54))
+
+
+
+print("What type of operation do you want to do? : ")
+op = input()
+print("You picked: (" + op + ") correct?: [Y/N]]" )
+con = input()
+con = con.lower()
+
+if con == "y":
+    print("YOU selected yes")
+
+
